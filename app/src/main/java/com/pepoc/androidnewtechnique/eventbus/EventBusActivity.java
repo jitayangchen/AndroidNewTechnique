e0f8313bcbd3945384c4b42146eee902cd32a170
Yangchen <jitayangchen@gmail.com>
2016-01-11 18:33:08 +0800

    add unsubscribe

--- app/src/main/java/com/pepoc/androidnewtechnique/eventbus/EventBusActivity.java
+++ app/src/main/java/com/pepoc/androidnewtechnique/eventbus/EventBusActivity.java
@@ -1,31 +1,30 @@
 package com.pepoc.androidnewtechnique.eventbus;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.pepoc.androidnewtechnique.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class EventBusActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_bus);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        init();
+    }
+
+    private void init() {
+        setSupportActionBar(toolbar);
     }
 
 }
--- app/src/main/java/com/pepoc/androidnewtechnique/rxbus/RxBusActivity.java
+++ app/src/main/java/com/pepoc/androidnewtechnique/rxbus/RxBusActivity.java
@@ -3,6 +3,7 @@ package com.pepoc.androidnewtechnique.rxbus;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ import com.pepoc.androidnewtechnique.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import rx.Subscriber;
 
 public class RxBusActivity extends AppCompatActivity {
 
@@ -18,6 +20,9 @@ public class RxBusActivity extends AppCompatActivity {
     Toolbar toolbar;
     @Bind(R.id.btn_test)
     Button btnTest;
+    @Bind(R.id.btn_unsubscribe)
+    Button btnUnsubscribe;
+
     private RxBus rxBus;
 
     @Override
@@ -37,10 +42,44 @@ public class RxBusActivity extends AppCompatActivity {
 
         RxView.clicks(btnTest).subscribe(aVoid -> rxBus.send("Yangchen"));
 
-        rxBus.toObserverable().subscribe(o -> {
-            Toast.makeText(RxBusActivity.this, o.toString(), Toast.LENGTH_SHORT).show();
+//        rxBus.toObserverable().subscribe(o -> {
+//            Toast.makeText(RxBusActivity.this, o.toString(), Toast.LENGTH_SHORT).show();
+//        });
+
+        Subscriber subscriber = new TestSubscriber<Object>() {
+            @Override
+            public void onNext(Object o) {
+                Toast.makeText(RxBusActivity.this, o.toString(), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        rxBus.toObserverable().subscribe(subscriber);
+
+        RxView.clicks(btnUnsubscribe).subscribe(aVoid -> {
+            Log.i("LLL", "---------------- " + subscriber.isUnsubscribed());
+            if (!subscriber.isUnsubscribed()) {
+                subscriber.unsubscribe();
+            }
         });
 
+    }
+
+    class TestSubscriber<T> extends Subscriber<T> {
+
+        @Override
+        public void onCompleted() {
+
+        }
+
+        @Override
+        public void onError(Throwable e) {
+
+        }
+
+        @Override
+        public void onNext(T t) {
+
+        }
     }
 
 }
